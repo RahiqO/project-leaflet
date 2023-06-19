d7c108fef599690ce5dea5fda7cd200b35f89c2b
RahiqO <Rahiq.a.osman@gmail.com>
2023-06-19 18:32:55 -0400

    .

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,43 +1,99 @@
 // We create the tile layer that will be the background of our map
-  let streetmap =   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  let basemapLayer =   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
 // We create the map object with options.
   let baseMaps = {
-    "Street Map": streetmap
+    "Street Map": basemapLayer
   };
-  let overlayMaps = {
-    "Earth quake location": earthquake
-    };
+  // let overlayMaps = {
+  //   "Earth quake location": earthquake
+  //   };
 
-  let Mymap = L.Mymap("map", {
+  let Mymap = L.map("map", {
       center: [37.09024, -95.712891],
       zoom: 4,
-      layers: [streetmap, earthquake]
+      layers: [basemapLayer]
     });
-  L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(map);
+
+    // basemapLayer.addTo(map);
+
+    fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) { console.log(data)
+    });
+
+  function getEarthquakeStyle(feature) {
+  // Extract the magnitude value from the properties
+    let magnitude = feature.geometry.coordinates[3];
+    var color = getColor(magnitude);
+    var radius = getRadius(magnitude);
+
+    let earthmarkers = [];
+
+    for (let index = 0; index < magnitude.length; index++) {
+        let magnitude = magnitude[index];
+    
+      
+        var style = {
+          radius: radius(magnitude),
+          fillColor: color(magnitude),
+          color: '#ffffff',
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.7
+        };
+      
+        return style;
+      }
+      
+function getColor(magnitude) {
+    // Define color logic based on magnitude ranges
+  if (magnitude < 4) {
+  return '#ff0000'; // Red for low magnitudes
+  } else {
+    return '#00ff00'; // Green for high magnitudes
   }
-  
-  
-  function createMarkers(response) {
+}}
 
-    let stations = response.metadata.geometry;
+function getEarthquakcoords(featurecoords) {
 
-    let earthquakeMarkers = [];
+  // Extract the magnitude value from the propertie
 
-    for (let index = 0; index < stations.length; index++) {
-      let station = stations[index];
-      let earthquakeMarkers = L.marker([station.coordinates[0], station.coordinates[1]])
-      .bindPopup("<h3>" + station.title + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-
-      earthquakeMarkers.push(earthquakeMarkers);
+    for (let index = 0; index < magnitude.length; index++) {
+      let magnitude = magnitude[index];
     }
 
-    createMap(L.layerGroup(earthquakeMarker));
+// function getRadius(magnitude) {
+//   // Define radius logic based on magnitude ranges
+//   return magnitude * 2;
+
+    
+  // L.control.layers(baseMaps, overlayMaps, {
+  //     collapsed: false
+  //   }).addTo(map);
+  // }
   
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
+  
+  // function createMarkers(response) {
+
+  //   let stations = response.metadata.geometry;
+
+  //   let earthquakeMarkers = [];
+
+  //   for (let index = 0; index < stations.length; index++) {
+  //     let station = stations[index];
+  //     let earthquakeMarkers = L.marker([station.coordinates[0], station.coordinates[1]])
+  //     .bindPopup("<h3>" + station.title + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+
+  //     earthquakeMarkers.push(earthquakeMarkers);
+  //   }
+
+  //   createMap(L.layerGroup(earthquakeMarker));
+  
+  // d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
   
   // fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
   // .then(function(response) {
